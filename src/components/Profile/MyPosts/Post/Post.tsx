1b95adf16e5fc5df828d7a3e57154da658b55c34
Ruslan <ruslan.pershin98@gmail.com>
2022-05-10 15:11:39 +0300

    18: page Dialogs

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import s from './Post.module.css';
 
 type PostMessageType = {
-    message: string
+    message: string,
+    likesCount: number
 }
 
 function Post(props: PostMessageType) {
     return (
         <div className={s.item}>
-            <img src="https://i.pinimg.com/originals/ff/a0/9a/ffa09aec412db3f54deadf1b3781de2a.png" alt=""/>
+            <img src="https://i.pinimg.com/originals/ff/a0/9a/ffa09aec412db3f54deadf1b3781de2a.png" alt="avatar"/>
             {props.message}
             <div>
-                <span>like</span>
+                <span>like</span> {props.likesCount}
             </div>
         </div>
     );
