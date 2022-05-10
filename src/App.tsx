99e1a45b0b0457d0cedb56af0fa5a51cc5fe84d5
Ruslan <ruslan.pershin98@gmail.com>
2022-05-10 16:02:34 +0300

    20: NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
                 <div className='app-wrapper-content'>
                     <Route path='/profile' component={Profile}/>
                     <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
---
+++ src/components/Music/Music.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Music.module.css';
+
+function Music() {
+    return (
+        <div>
+            music
+        </div>
+    );
+}
+
+export default Music;
