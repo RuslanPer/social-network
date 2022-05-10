dd1194b07c002c164445c227897026e249440e1d
Ruslan <ruslan.pershin98@gmail.com>
2022-05-10 19:03:29 +0300

    25: map data posts, dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -27,7 +27,7 @@ function Message(props: MessagePropsType) {
 
 function Dialogs() {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Dimych'},
         {id: 2, name: 'Andrey'},
         {id: 3, name: 'Sveta'},
@@ -36,21 +36,22 @@ function Dialogs() {
         {id: 6, name: 'Valera'},
     ];
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How are you?'},
         {id: 3, message: 'Yo'},
     ];
 
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = messages.map( m => <Message message={m.message}/>);
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
+                {messagesElements}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,10 +4,12 @@ import Post from "./Post/Post";
 
 function MyPosts() {
 
-    let postData = [
+    let posts = [
         {id: 1, message: "Hi, how are you?", likesCount: 0},
         {id: 2, message: "It's my first post", likesCount: 5},
     ];
+
+    let postElements = posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>)
 
     return (
         <div className={s.postsBlock}>
@@ -22,8 +24,7 @@ function MyPosts() {
 
             </div>
             <div className={s.posts}>
-                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
-                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
+                {postElements}
             </div>
         </div>
     );
