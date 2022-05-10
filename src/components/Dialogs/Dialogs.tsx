e86213d7cab472705dc7c50076cf4b9120813ca0
Ruslan <ruslan.pershin98@gmail.com>
2022-05-10 18:22:05 +0300

    24: UI - BLL, we take the data to the side

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -26,20 +26,31 @@ function Message(props: MessagePropsType) {
 }
 
 function Dialogs() {
+
+    let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Victor'},
+        {id: 6, name: 'Valera'},
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Yo'},
+    ];
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={'Dimych'} id={1}/>
-                <DialogItem name={'Andrey'} id={2}/>
-                <DialogItem name={'Sveta'} id={3}/>
-                <DialogItem name={'Sasha'} id={4}/>
-                <DialogItem name={'Victor'} id={5}/>
-                <DialogItem name={'Valera'} id={6}/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
             </div>
             <div className={s.messages}>
-                <Message message={'Hi'}/>
-                <Message message={'How are you?'}/>
-                <Message message={'Yo'}/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,12 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 function MyPosts() {
+
+    let postData = [
+        {id: 1, message: "Hi, how are you?", likesCount: 0},
+        {id: 2, message: "It's my first post", likesCount: 5},
+    ];
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -16,8 +22,8 @@ function MyPosts() {
 
             </div>
             <div className={s.posts}>
-                <Post message={"Hi, how are you?"} likesCount={0}/>
-                <Post message={"It's my first post"} likesCount={5}/>
+                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
             </div>
         </div>
     );
