32d52acbc9aeedec0cd571cd53441507c8441428
Ruslan <ruslan.pershin98@gmail.com>
2022-05-10 13:49:59 +0300

    16: props message Post

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import s from './Post.module.css';
 
-function Post() {
+type PostMessageType = {
+    message: string
+}
+
+function Post(props: PostMessageType) {
     return (
         <div className={s.item}>
             <img src="https://i.pinimg.com/originals/ff/a0/9a/ffa09aec412db3f54deadf1b3781de2a.png" alt=""/>
-            post 1
+            {props.message}
             <div>
                 <span>like</span>
             </div>
