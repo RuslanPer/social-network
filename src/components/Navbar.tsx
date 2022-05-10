aa4b64af2c28e919e13c89252460d864ec9983e4
Ruslan <ruslan.pershin98@gmail.com>
2022-05-10 11:10:41 +0300

    lesson 12; add: Header, Navbar, Profile components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function Navbar() {
+    return (
+        <nav className='nav'>
+            <div><a href="#"></a>Profile</div>
+            <div><a href="#"></a>Messages</div>
+            <div><a href="#"></a>News</div>
+            <div><a href="#"></a>Music</div>
+            <div><a href="#"></a>Settings</div>
+        </nav>
+    );
+}
+
+export default Navbar;
